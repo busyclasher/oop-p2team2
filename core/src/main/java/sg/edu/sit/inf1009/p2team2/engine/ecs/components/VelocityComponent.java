9b62f09e7f6b84d3c00a1d89c5f18457ff75d4cd
Muhammad Hasif Bin Mohd Faisal <hasif@Muhammads-MacBook-Pro-5.local>
2026-02-04 13:06:44 +0800

    ecs: add VelocityComponent stub

---
+++ core/src/main/java/sg/edu/sit/inf1009/p2team2/engine/ecs/components/VelocityComponent.java
@@ -0,0 +1,31 @@
+package sg.edu.sit.inf1009.p2team2.engine.ecs.components;
+
+import com.badlogic.gdx.math.Vector2;
+import sg.edu.sit.inf1009.p2team2.engine.ecs.Component;
+
+public class VelocityComponent implements Component {
+    private Vector2 velocity;
+    private Vector2 acceleration;
+
+    public VelocityComponent() {
+        this.velocity = new Vector2();
+        this.acceleration = new Vector2();
+    }
+
+    public Vector2 getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(Vector2 velocity) {
+        this.velocity = velocity;
+    }
+
+    public Vector2 getAcceleration() {
+        return acceleration;
+    }
+
+    public void setAcceleration(Vector2 acceleration) {
+        this.acceleration = acceleration;
+    }
+}
+
